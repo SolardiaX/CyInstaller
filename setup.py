4ad41bb2d84271a9718193070ec698c4d8dfec34
SolardiaX <solardiax@hotmail.com>
2024-04-01 16:37:49 +0800

    feat: tag v1.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import re
 
 from setuptools import setup
 
-with open("src/cyinstaller/__init__.py", encoding="utf8") as f:
+with open("src/CyInstaller/__init__.py", encoding="utf8") as f:
     version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
@@ -11,9 +11,9 @@ setup(
     version=version,
     install_requires=[
         "python>=3.8,<3.13"
-        "click>=8.1.6",
-        "cython>=3.0.0",
-        "pyinstaller>=5.13.0",
+        "click>=8.1.7",
+        "cython>=3.0.10",
+        "pyinstaller>=6.5.0",
         "pyyaml>=6.0.1",
     ]
 )
